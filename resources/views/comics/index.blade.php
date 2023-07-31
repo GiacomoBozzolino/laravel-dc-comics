4525307b4982d1313f0faabfa83b05c5de9bbeac
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-31 16:04:00 +0200

    [FEAT] fatto crud index

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,14 @@
+ 
+ @foreach($comics as $comic)
+    <div class="col-2">
+        <li class="d-flex pt-5 pb-2">
+            <a href="">
+            {{-- <a href="{{route('comic.show', $comic->id)}}"> --}}
+                <img class="thumb" src="{{$comic ->thumb }}" alt="{{$comic ->title }}">
+                <div >
+                {{$comic ->title }}
+                </div>
+            </a>
+        </li> 
+    </div>
+@endforeach 
