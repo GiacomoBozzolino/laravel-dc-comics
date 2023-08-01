303abe1c308f90974b22e6effde1f2bfca8f78e1
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-01 16:20:18 +0200

    [FEAT] aggiunto pulsate metodo destroy

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -11,7 +11,21 @@
                 </a>
 
             </div>
-            <a  class="btn btn-warning btn-sm" href="{{route('comics.edit', $comic->id)}}"><i class="fas fa-edit"></i></a> 
+            <div class="d-flex">
+                {{-- bottone per modifica --}}
+                <a  class="btn btn-warning btn-sm me-2" href="{{route('comics.edit', $comic->id)}}">
+                    <i class="fas fa-edit"></i>
+                </a> 
+                {{-- bottone per cancellazione (dentro form) --}}
+                <form action="{{route('comics.destroy', $comic->id)}}" method="post" >
+                    @csrf
+                    @method ('DELETE')
+                    <a  class="btn btn-danger btn-sm" href="{{route('comics.edit', $comic->id)}}">
+                        <i class="fas fa-trash"></i>
+                    </a> 
+    
+                </form>
+            </div>
         </li> 
     </div>
 @endforeach 
