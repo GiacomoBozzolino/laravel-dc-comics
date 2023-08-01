97cd4f1cbe018b31390158cd942c616aab0f09a1
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-01 16:41:56 +0200

    [FEAT] aggiunta funzione old a form in create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -19,24 +19,23 @@
                     @csrf
                     <div class="form-group">
                         <label class="control-label" >Title</label>
-                        <input type="text" id="title" name="title" class="form-control" placeholder="Title">
+                        <input type="text" id="title" name="title" class="form-control" placeholder="Title" value="{{old('title')}}">
                     </div>
                     <div class="form-group">
                         <label class="control-label" >Thumbnail</label>
-                        <input type="text" id="thumb" name="thumb" class="form-control" placeholder="thumb">
+                        <input type="text" id="thumb" name="thumb" class="form-control" placeholder="thumb" value="{{old('thumb')}}">
                     </div>
                     <div class="form-group">
                         <label class="control-label" >Description</label>
-                      
-                        <textarea name="description" id="description" cols="30" rows="5" class="form-control" placeholder="description"></textarea>
+                        <textarea name="description" id="description" cols="30" rows="5" class="form-control" placeholder="description">{{old('description')}}</textarea>
                     </div>
                     <div class="form-group">
                         <label class="control-label" >Price</label>
-                        <input type="text" id="price" name="price" class="form-control" placeholder="price">
+                        <input type="text" id="price" name="price" class="form-control" placeholder="price" value="{{old('price')}}">
                     </div>
                     <div class="form-group">
                         <label class="control-label" >Series</label>
-                        <input type="text" id="Series" name="series" class="form-control" placeholder="Series">
+                        <input type="text" id="Series" name="series" class="form-control" placeholder="Series" value="{{old('series')}}">
                     </div>
                     <div class=" form-group mt-2">
                         <button type="submit" clas="btn btn-primary"> Salva</button>
